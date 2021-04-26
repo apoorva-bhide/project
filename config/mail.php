5db4fdc4d884a232ab98efbc1e382ea8be88b62b
apoorva06 <apoorvabhide2@gmail.com>
2021-04-26 18:05:10 +0530

    Gmail changed

--- config/mail.php
+++ config/mail.php
@@ -92,7 +92,7 @@ return [
     ],
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'bhideapurva7@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'bapurva87@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Laravel'),
     ],
 
